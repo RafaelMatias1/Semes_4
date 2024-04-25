dc326e31395e4f044450a50732a6dee275ce09f3
Rafael Matias <rafamatiasschmidt@gmail.com>
2024-04-25 16:26:21 -0300

    25/04

---
+++ React/SA/P1_SA/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InfPagamento from './components/InfPagamento';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InfPagamento />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/SA/P1_SA/src/components/InfPagamento.jsx
@@ -0,0 +1,20 @@
+import './InfPagamento.css';
+
+function InfPagamento () {
+
+  return (
+    <div>
+      <div className="container">
+        <div className='cima'>
+          
+        </div>
+        <div className='meio'>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default InfPagamento;
