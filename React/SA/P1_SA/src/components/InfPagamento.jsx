286c0ece54f262652e226edfbc1c4f280e3afa16
Rafael Matias <rafamatiasschmidt@gmail.com>
2024-04-29 14:38:38 -0300

    29/04

--- React/SA/P1_SA/src/components/InfPagamento.jsx
+++ React/SA/P1_SA/src/components/InfPagamento.jsx
@@ -12,13 +12,38 @@ function InfPagamento () {
         </div>
         <div className='meio'>
           <div className='local'>
-            <div className='input'><input placeholder='Nome Completo' className='input1'/><input placeholder='CEP' className='input2'/></div>
-            <div className='input'></div>
-            <div className='input'></div>
-            <div className='input'></div>
+
+            <div className='input'>
+
+              <input placeholder='Nome Completo' className='input1'/><input placeholder='CEP' className='input2'/>
+            </div>
+            <div className='input'>
+
+              <input placeholder='Estado' className='input3'/><input placeholder='Cidade' className='input4'/>
+
+            </div>
+            <div className='input'>
+
+              <input placeholder='Bairro' className='input3'/><input placeholder='Numero da Rua' className='input4'/>
+
+            </div>
+            <div className='input'>
+
+              <input placeholder='DDD' className='input5'/><input placeholder='Telefone' className='input6'/>
+
+            </div>
+
           </div>
           <div className='historico'>
+            <div className='compra'>
+              <div className='produto'>
 
+              </div>
+            </div>
+            <div className='botao_compra'>
+              <div className='buton'>
+              </div>
+            </div>
           </div>
         </div>
 
