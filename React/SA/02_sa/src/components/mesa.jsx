c71609ee008fb371feded82743586de8543ae646
Rafael Matias <rafamatiasschmidt@gmail.com>
2024-05-07 16:14:10 -0300

    07/05

--- React/SA/02_sa/src/components/mesa.jsx
+++ React/SA/02_sa/src/components/mesa.jsx
@@ -1,54 +1,47 @@
+import React, { useState, useEffect } from 'react';
 import './mesa.css';
-//import imagem_carrosel from './imagem/imagem_cfe'
+import imagem_combo from './imagem/combo.png'
+import imagem_combo2 from './imagem/combo02.png'
+import imagem_combo3 from './imagem/combo03.png'
 
 function mesa () {
+
+  const funcaoBotao = () => {
+    
+  };
+
   const [index, setIndex] = useState(0);
-  const slides = document.querySelectorAll('.carousel-slide');
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setIndex(prevIndex => {
-        let newIndex = prevIndex + 1;
-        if (newIndex >= slides.length) {
-          newIndex = 0; 
-        }
-        return newIndex;
-      });
+      setIndex(prevIndex => (prevIndex + 1) % 3); // Assumindo que há 3 imagens
     }, 3000);
 
     return () => {
-      clearInterval(intervalId); 
+      clearInterval(intervalId);
     };
   }, []);
 
-  useEffect(() => {
-    slides.forEach((slide, i) => {
-      if (i === index) {
-        slide.style.display = 'block';
-      } else {
-        slide.style.display = 'none';
-      }
-    });
-  }, [index]);
-
   return (  
     <div className='container'>
+      <div className='esquerda'>
+        <button onClick={funcaoBotao} className='botao'>Menu</button>
+        <button onClick={funcaoBotao} className='botao'>Combos</button>
+        <button onClick={funcaoBotao} className='botao'>Comanda</button>
+      </div>
       <div className='direita'>
 
-      </div>
-      <div className='esquerda'>
-
-          <div class="carousel-container">
-            <div class="carousel-slide">
-              <img src={imagem_carrosel} className='imagem_cafe' /> 
-            </div>
-              <div class="carousel-slide">
-            <img src={imagem_carrosel} className='imagem_cafe' /> 
-            </div>
-            <div class="carousel-slide">
-              <img src={imagem_carrosel} className='imagem_cafe' /> 
-            </div>
+        <div className="carousel-container">
+          <div className="carousel-slide" style={{ display: index === 0 ? 'block' : 'none' }}>
+            <img src={imagem_combo} className='imagem_cafe' alt="Combo 1" /> 
           </div>
+          <div className="carousel-slide" style={{ display: index === 1 ? 'block' : 'none' }}>
+            <img src={imagem_combo2} className='imagem_cafe' alt="Combo 2" /> 
+          </div>
+          <div className="carousel-slide" style={{ display: index === 2 ? 'block' : 'none' }}>
+            <img src={imagem_combo3} className='imagem_cafe' alt="Combo 3" /> 
+          </div>
+        </div>
 
       </div>
     </div>
